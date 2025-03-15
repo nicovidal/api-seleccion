07b8919b2828c1ae73a06a7b4c4e33778d4eaef3
nicovidal <nico.vidal.m@gmail.com>
2025-03-15 13:02:56 -0300

    avance seleccion

--- src/seleccion/seleccion.service.ts
+++ src/seleccion/seleccion.service.ts
@@ -1,10 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { ObtencionDataService } from 'src/obtencion-data/obtencion-data.service';
 
+
+interface SeleccionFinalResponse {
+  cliente?: any;
+  deuda?: any;
+  mensaje?: string;
+  elegible?: boolean;
+}
+
+
 @Injectable()
 export class SeleccionService {
 
-  constructor(private readonly obtencionDataService: ObtencionDataService) {} 
+  constructor(private readonly obtencionDataService: ObtencionDataService) { }
+
+
+  async seleccionFinal(rut: string) {
+
+    const seleccionFinalResponse: SeleccionFinalResponse = {};
+
+    this.seleccionCliente(rut)
+
+
+    const deudaCliente = await this.seleccionDeuda(rut)
+
+    if (deudaCliente) {
+      throw new Error('Cliente con deuda , no puede optar a tarjeta')
+    }
+
+    seleccionFinalResponse.deuda='No tiene deuda'
+
+    return seleccionFinalResponse;
+  }
+
+
 
   async seleccionCliente(rut: string) {
     try {
@@ -15,4 +45,19 @@ export class SeleccionService {
       throw new Error('Error al obtener datos del cliente');
     }
   }
+
+  async seleccionDeuda(rut: string) {
+    try {
+      const clienteDatos = await this.obtencionDataService.obtenerDeudaCliente(rut);
+      console.log('Datos de deuda:', clienteDatos);
+      return clienteDatos;
+    } catch (error) {
+      throw new Error('Error al obtener deudas datos del cliente');
+    }
+  }
+
+
+
+
+
 }
